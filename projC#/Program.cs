df17c608974b8082ec4209b2f0e913396140e677
Alexey <lexsys2014@hotmail.com>
2022-12-03 19:22:58 +0300

    Добавлены решения задач по другим урокам

--- projC#/Program.cs
+++ projC#/Program.cs
@@ -3,10 +3,23 @@ twoval();
 threeval();
 provVal();
 outVal();
+secondVal();
+thirdVal();
+dayWeek();
 //функция для ввода целых чисел
 int inputdata(int x){
-    Console.Write("Input value "+x+": ");
-    int val=Convert.ToInt32(Console.ReadLine());
+    bool prov=false;
+    int val=0;
+    while (!prov){
+        try{
+            Console.Write("Input value "+x+": ");
+            val=Convert.ToInt32(Console.ReadLine());
+            prov=true;
+        }
+        catch{
+            Console.WriteLine("Error numbers");
+        }
+    }
     return val;
 }
 //Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
@@ -15,9 +28,9 @@ void twoval(){
     int val1=inputdata(1);
     int val2=inputdata(2);
     if(val1>val2){
-        Console.Write("Max value "+val1+", Min value"+val2+"\n");
+        Console.WriteLine("Max value "+val1+", Min value"+val2);
     }else{
-        Console.Write("Max value "+val2+", Min value "+val1+"\n");
+        Console.WriteLine("Max value "+val2+", Min value "+val1);
     }
 }
 //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
@@ -32,16 +45,16 @@ void threeval(){
         if(maxVal<val[i])
             maxVal=val[i];
     }
-    Console.Write("Max value "+maxVal+"\n");
+    Console.WriteLine("Max value "+maxVal+"\n");
 }
 //Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 void provVal(){
     Console.WriteLine("Task 3:");
     int val=inputdata(1);
     if(val % 2==0){
-        Console.Write("Yes\n");
+        Console.WriteLine("Yes");
     }else{
-        Console.Write("No\n");
+        Console.WriteLine("No");
     }
 }
 //Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
@@ -53,4 +66,46 @@ void outVal(){
             Console.Write(i+" ");
         }
     }
+    Console.Write("\n");
+}
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+void secondVal(){
+    Console.WriteLine("Task 5:");
+    bool prov=false;
+    int val=0;
+    while (!prov){
+        try{
+            val=inputdata(1);
+            if (val>99 && val<1000)
+                prov=true;
+        }
+        catch{
+            Console.WriteLine("Error numbers");
+        }
+    }
+    string str=Convert.ToString(val);
+    Console.WriteLine(str[1]);
+}
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+void thirdVal(){
+    Console.WriteLine("Task 6:");
+    int val=inputdata(1);
+    string str=Convert.ToString(val);
+    if(str.Length>=3 && str[0]!='-')
+        Console.WriteLine(str[2]);
+    else if(str.Length>3 && str[0]=='-')
+        Console.WriteLine(str[3]);
+    else
+        Console.WriteLine("Not third number");
+}
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+void dayWeek(){
+    Console.WriteLine("Task 7:");
+    int val=inputdata(1);
+    if(val<=7 && val>=6)
+        Console.WriteLine("Yes");
+    else if(val<=5 && val>=1)
+        Console.WriteLine("No");
+    else
+        Console.WriteLine("Error number");
 }
